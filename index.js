57a365f659b6f033a6a6477f4b1c8eb34c124a64
giulia-demuro <54404751+giulia-demuro@users.noreply.github.com>
2023-04-03 11:35:02 +0200

    3 files modified, 2 added

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 import { flights } from './data';
+import { getTickets } from './tickets.js';
 import './style.css';
 
 const appDiv = document.getElementById('app');
+
+const container = document.createElement('div');
+container.setAttribute('id', 'container');
+container.classList.add('d-flex');
+
+const ticketsList = document.createElement('ul');
+const cart = document.createElement('div');
+
+const getTickets = () => {
+  flights.forEach((item) => {
+    const ticket = document.createElement('li');
+    ticket.setAttribute('class', 'ticket');
+
+    ticket.innerText = 'Ticket';
+    ticketsList.appendChild(ticket);
+  });
+
+  ticketsList.setAttribute('id', 'tickets');
+  ticketsList.classList.add('d-flex-col');
+};
+
+const createCart = () => {
+  cart.setAttribute('id', 'cart');
+  const cartHeader = document.createElement('div');
+  cartHeader.setAttribute('id', 'cart-header');
+
+  cart.appendChild(cartHeader);
+  cart.innerText = 'Cart';
+};
+
+getTickets();
+createCart();
+
+container.append(ticketsList, cart);
+appDiv.appendChild(container);
---
+++ tickets.js
@@ -0,0 +1,12 @@
+const ticketsList = document.createElement('ul');
+export const getTickets = (flights) => {
+  flights.forEach((item) => {
+    const ticket = document.createElement('li');
+    ticket.innerText = 'Ticket';
+
+    ticketsList.appendChild(ticket);
+  });
+  
+  ticketsList.setAttribute('id', 'tickets');
+  return ticketsList;
+};
